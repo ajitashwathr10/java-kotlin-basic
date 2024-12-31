829086a21220fc53a804b9c1555f92c85e7eba62
{Ajit <ajitashwathr23@lpu.in>
2024-12-31 15:19:00 +0530

    Update App.java

--- Java/number-guess/src/App.java
+++ Java/number-guess/src/App.java
@@ -1,5 +1,38 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int numberToGuess = random.nextInt(100) + 1;
+        int attempts = 0;
+        boolean hasWon = false;
+
+        System.out.println("Number Guessing Game");
+        System.out.println("Think a number between 1 and 100");
+        while(!hasWon && attempts < 10) {
+            System.out.println("Enter your no.: ");
+            try {
+                int guess = scanner.nextInt();
+                attempts++;
+                if(guess == numberToGuess) {
+                    hasWon = true;
+                    System.out.println("You guessed it in " + attempts + " attempts");
+                } else if(guess < numberToGuess) {
+                    System.out.println("Too low! Try again");
+                } else {
+                    System.out.println("Too high! Try again");
+                }
+            } catch(Exception e) {
+                System.out.println("Please enter a valid number");
+                scanner.nextLine();
+            }
+        }
+        if(!hasWon) {
+            System.out.println("Game over! The number was: " + numberToGuess);
+        }
+        scanner.close();
     }
 }
